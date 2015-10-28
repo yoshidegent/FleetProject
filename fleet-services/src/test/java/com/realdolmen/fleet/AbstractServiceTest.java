b3df31cb89471403c4e51575d9fe7dfa252989f3
YDEAX41 <yoshi_degent@hotmail.com>
2015-10-28 14:03:19 +0100

    changed profile to test

--- fleet-services/src/test/java/com/realdolmen/fleet/AbstractServiceTest.java
+++ fleet-services/src/test/java/com/realdolmen/fleet/AbstractServiceTest.java
@@ -6,11 +6,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = { ServicesTestConfig.class })
 @ActiveProfiles("test")
-@Transactional
 public abstract class AbstractServiceTest {
 }
