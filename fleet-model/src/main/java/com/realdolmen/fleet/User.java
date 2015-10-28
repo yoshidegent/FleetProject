695e66a784c7a23012a923d96c12b07aed9b6b85
Dean Wyns <dean.wyns@outlook.com>
2015-10-28 14:26:50 +0100

    ...

--- fleet-model/src/main/java/com/realdolmen/fleet/User.java
+++ fleet-model/src/main/java/com/realdolmen/fleet/User.java
@@ -1,9 +1,12 @@
 package com.realdolmen.fleet;
 
+import org.hibernate.validator.constraints.Email;
+
 import javax.persistence.Entity;
 
 @Entity
 public abstract class User extends AbstractEntity {
+    @Email
     private String email;
     private String password;
 }
