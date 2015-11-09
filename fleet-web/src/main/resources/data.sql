fd52c194a17bba0696a5030e72a636d00dccadb8
YDEAX41 <yoshi_degent@hotmail.com>
2015-11-09 14:15:59 +0100

    commit employee

--- fleet-web/src/main/resources/data.sql
+++ fleet-web/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO `fleet`.`user` (`DTYPE`, `id`, `version`, `email`, `password`, `dateOfBirth`, `firstName`, `function`, `functionalLevel`, `hireDate`, `lastName`, `isActive`) VALUES ('Employee', '1', '1', 'yoshi.degent@realdolmen.com', '$2a$04$4.QXaVbhnR7MR58foRJxROca5yY1qLvN2KkZFdo980vpCep3P5/Lm', NOW(), 'Yoshi', 'Developer', '5', NOW(), 'Degent', 1);
-INSERT INTO `fleet`.`user` (`DTYPE`, `id`, `version`, `email`, `password`, `dateOfBirth`, `firstName`, `function`, `functionalLevel`, `hireDate`, `lastName`, `isActive`) VALUES ('Employee', '2', '1', 'dean.wyns@realdolmen.com', '$2a$04$4.QXaVbhnR7MR58foRJxROca5yY1qLvN2KkZFdo980vpCep3P5/Lm', NOW(), 'Dean', 'Developer', '2', NOW(), 'Wyns', 1);
+INSERT INTO `fleet`.`user` (`DTYPE`, `id`, `version`, `email`, `password`, `dateOfBirth`, `firstName`, `function`, `functionalLevel`, `hireDate`, `lastName`, `active`) VALUES ('Employee', '1', '1', 'yoshi.degent@realdolmen.com', '$2a$04$4.QXaVbhnR7MR58foRJxROca5yY1qLvN2KkZFdo980vpCep3P5/Lm', NOW(), 'Yoshi', 'Developer', '5', NOW(), 'Degent', 1);
+INSERT INTO `fleet`.`user` (`DTYPE`, `id`, `version`, `email`, `password`, `dateOfBirth`, `firstName`, `function`, `functionalLevel`, `hireDate`, `lastName`, `active`) VALUES ('Employee', '2', '1', 'dean.wyns@realdolmen.com', '$2a$04$4.QXaVbhnR7MR58foRJxROca5yY1qLvN2KkZFdo980vpCep3P5/Lm', NOW(), 'Dean', 'Developer', '2', NOW(), 'Wyns', 1);
 
 INSERT INTO `fleet`.`user_roles` (`User_id`, `roles`) VALUES ('1', 'Employee');
 INSERT INTO `fleet`.`user_roles` (`User_id`, `roles`) VALUES ('2', 'Employee');
